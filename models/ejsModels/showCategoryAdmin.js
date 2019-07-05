1d52eec064a4e20d28201dabda627981a334563a
HoangBuiThai <35281302+HoangBuiThai@users.noreply.github.com>
2019-07-05 19:56:39 +0700

    Last-update-pp

--- models/ejsModels/showCategoryAdmin.js
+++ models/ejsModels/showCategoryAdmin.js
@@ -23,9 +23,10 @@ var show = async(n) => {
     for (let i = 0; i < n.length; i++) {
 
         await countNews(n[i]._id).then(rs => count = rs).catch(err => console.log(err));
-        kq += "<div class=\"col-xl-6\" style=\"border:2px solid green;border-radius:15px;margin:10px\">";
+        kq += "<div class=\"col-xl-6\" style=\"border:2px solid green;border-radius:15px;margin:10px;display:inline-block;\">";
         kq += "<h2 style=\"display:inline-block;\"><a href=\"./admin/category/" + String(n[i]._id) + "\">" + n[i].title + "</a></h2>";
         kq += "<span style=\"float:right;font-size:25px;\">" + count + " bài</span></div>";
+        kq += "<button class=\"btn btn-outline-secondary btn-edit\"><a href=\"/login/admin/category/delete/" + n[i]._id + "/" + count + "\"+>Xóa</a></button>";
     }
     kq += "</div>";
     return kq;
